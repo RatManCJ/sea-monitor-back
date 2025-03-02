50e254b15fd9ff49f3685aee00586f3b90a66d18
jingc <cjingc368@gamil.com>
2025-03-02 15:39:46 +0800

    初始化SpringBoot项目

---
+++ src/main/java/org/jingc/SeaMonitorApplication.java
@@ -0,0 +1,11 @@
+package org.jingc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaMonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SeaMonitorApplication.class, args);
+    }
+}
